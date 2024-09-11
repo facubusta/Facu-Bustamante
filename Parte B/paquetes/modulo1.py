36d2da40e7b8a31dd7722e53413d7a1e93912a6a
Facundo Bustamante <facubustamanteok@gmail.com>
2024-09-11 11:19:20 -0300

    Correcion de codigo

--- Parte B/paquetes/modulo1.py
+++ Parte B/paquetes/modulo1.py
@@ -1,11 +1,14 @@
-# Devuelve el valor total sumando los impuestos
-def calcular_impuestos(valor_exportaciones, valor_ventas_nacionales, iva = 21, retenciones = 15):
-    resultado_nacional = valor_ventas_nacionales + calcular_porcentaje(valor_ventas_nacionales, iva)
-    resultado_exportaciones = valor_exportaciones - calcular_porcentaje(valor_exportaciones, retenciones)
-    resultado_final = resultado_nacional + resultado_exportaciones
-    return resultado_final
+def calcular_nacional(valor_ventas_nacionales, iva = 21)->float:
+    ''' Devuelve el valor total nacional'''
+    resultado_nacional = valor_ventas_nacionales * (1 / (1 + (iva / 100)))
+    return resultado_nacional
 
-# Devuelve el porcentaje
-def calcular_porcentaje(importe, porcentaje):
-    resultado = importe * porcentaje / 100
-    return resultado
+def calcular_exportaciones(valor_exportaciones, retenciones = 15)->float:
+    ''' Devuelve el valor total exportado'''
+    resultado_exportaciones = valor_exportaciones * (1 - (retenciones / 100))
+    return resultado_exportaciones
+
+def calcular_impuestos(valor_exportaciones, valor_ventas_nacionales, iva = 21, retenciones = 15):
+    ''' Devuelve el valor total de ventas'''
+    total_calculo = calcular_exportaciones(valor_exportaciones, retenciones) + calcular_nacional(valor_ventas_nacionales, iva)
+    return total_calculo
--- Parte B/paquetes/modulo2.py
+++ Parte B/paquetes/modulo2.py
@@ -1,22 +1,20 @@
-from paquetes import modulo1
-
-#Devuele el salario sumando la antiguedad
-def calcular_salario(horas_trabajadas, antiguedad):
+def calcular_salario(horas_trabajadas, antiguedad)->float:
+    '''Devuele el salario sumando la antiguedad'''  
     salario = horas_trabajadas * 10
-    incremento_antiguedad = modulo1.calcular_porcentaje(salario, 3 * antiguedad)
+    incremento_antiguedad = salario * (3*antiguedad)/100
     salario_total = salario + incremento_antiguedad
     return salario_total
 
-#Devuelve la productividad del empleado
-def calcular_prod_empleado(artefactos_producidos, horas_trabajadas):
-        if horas_trabajadas > 0:
-            prod_empleado = artefactos_producidos / horas_trabajadas
-            return prod_empleado 
-        else:
-             return 0   
+def calcular_prod_empleado(artefactos_producidos, horas_trabajadas)->float:
+    '''Devuelve la productividad del empleado'''
+    if horas_trabajadas > 0:
+        prod_empleado = artefactos_producidos / horas_trabajadas
+        return prod_empleado 
+    else:
+        return 0   
         
-#Muestra en pantalla toda la informacion del empleado
-def reportar_inf_empleado(horas_trabajadas, antiguedad, artefactos_producidos, nombre : str, edad):
+def reportar_inf_empleado(horas_trabajadas, antiguedad, artefactos_producidos, nombre : str, edad)->str:
+     '''Muestra en pantalla toda la informacion del empleado'''
      salario = calcular_salario(horas_trabajadas, antiguedad)
      productividad = calcular_prod_empleado(artefactos_producidos, horas_trabajadas)
      print(f"La informacion del empleado es: \nNombre: {nombre}\nEdad: {edad}\nSalario: {salario}\nProductividad: {productividad}")
